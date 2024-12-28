77a014bd75bd4f35496be8d0d4e5132b9d70b10e
yuvraj-rathod-1202 <rathodyuvraj12022007>
2024-12-28 12:15:07 +0530

    change port

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("Server is running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
