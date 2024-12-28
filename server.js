86fca621ea88147f03ca9ee22f0fb814050efc30
yuvraj-rathod-1202 <rathodyuvraj12022007>
2024-12-28 11:34:21 +0530

    change origin

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const server = http.createServer(app);
 
 // Enable CORS
 app.use(cors({
-  origin: "http://localhost:3000", // Allow only your React app to access
+  origin: ["http://localhost:3000", "https://vcw-frontend.vercel.app"], // Allow only your React app to access
   methods: ["GET", "POST"], // Allowed HTTP methods
   credentials: true // Allow cookies if needed
 }));
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Allow React app
+    origin: ["http://localhost:3000", "https://vcw-frontend.vercel.app"], // Allow React app
     methods: ["GET", "POST"], // Same methods as above
     credentials: true,
   },
