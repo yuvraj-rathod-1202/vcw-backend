0a84458e5035eb038eb010e982716190d225636f
yuvraj-rathod-1202 <rathodyuvraj12022007>
2025-01-10 11:46:14 +0530

    change cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Your frontend URL
+    origin: ["http://localhost:3000", "https://vcw-frontend.vercel.app"], // Your frontend URL
     methods: ["GET", "POST"],
   },
 });
@@ -17,12 +17,13 @@ io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
   socket.on("join-room", ({ id }) => {
-    console.log(`User joined room: ${id}`);
+    console.log(`${socket.id} joined room: ${id}`);
     socket.join(id);
     socket.broadcast.to(id).emit("user-joined", socket.id);
   });
 
   socket.on("offer", (data) => {
+    console.log(`${socket.id} offer to ${data.to}`)
     socket.to(data.to).emit("offer", {
       from: socket.id,
       offer: data.offer,
@@ -30,6 +31,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("answer", (data) => {
+    console.log(`${socket.id} answer to ${data.to}`)
     socket.to(data.to).emit("answer", {
       from: socket.id,
       answer: data.answer,
@@ -37,6 +39,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("candidate", (data) => {
+    console.log(`${socket.id} candidate to ${data.to}`)
     socket.to(data.to).emit("candidate", {
       from: socket.id,
       candidate: data.candidate,
